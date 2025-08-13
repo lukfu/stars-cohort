c71cfe89df39ba5eed73d9fe551a3e60222e54d6
lukfu <fu.lukas99@gmail.com>
2025-08-13 16:20:32 +0200

    started implmentation of dynamic page content display

--- scripts/stars.js
+++ scripts/stars.js
@@ -1,11 +1,42 @@
-const pageOptions = [
-  "overview", "about us", "about the research", "publications"
+const pageOptions = [{
+    id: "overview",
+    title: "Overview"
+  }, 
+  {
+    id: "about-us",
+    title: "About Us"
+  }, 
+  {
+    id: "about-the-research",
+    title: "About the Research"
+  },
+  {
+    id: "publications",
+    title: "Publications"
+  }
 ]
 
-function getCurrentPage() {
+let currentMainPage;
 
-}
+function getClickID(clicked) {
+  currentMainPage = clicked;
+  console.log('click: ' + currentMainPage)
+  pageDisplay(currentMainPage)
+};
 
-function pageDisplay() {
+function pageDisplay(currentMainPage) {
+  let pageTitleHTML = `test`;
+  let pageContentHTML = ``;
 
+  pageOptions.forEach((pageOptions) => {
+    //console.log(pageOptions.id)
+    //console.log(currentMainPage)
+    //console.log(pageOptions.id === currentMainPage)
+    if (pageOptions.id === currentMainPage) {
+      pageTitleHTML = pageOptions.title;
+    }
+  })
+
+  document.querySelector('.js-main-title')
+    .innerHTML = pageTitleHTML;
 }
